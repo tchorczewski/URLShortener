694eb75cfc77fa8734c050c1ad61e4603dacc80d
tchorowski.bartosz@gmail.com <tchorowski.bartosz@gmail.com>
2025-02-09 23:40:12 +0100

    Celery docker fixes attempts,

--- Dockerfile
+++ Dockerfile
@@ -4,10 +4,8 @@ COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 COPY . .
 EXPOSE 5000
-CMD ["python", "app.py"]
-
-FROM postgres:17
+EXPOSE 5432
 ENV POSTGRES_USER=postgres
 ENV POSTGRES_PASSWORD=root
 ENV POSTGRES_DB=url_shortener
-EXPOSE 5432
+CMD ["python", "app.py"]
--- tools/celery.py
+++ tools/celery.py
@@ -1,15 +1,16 @@
 from celery import Celery, Task
 from flask import Flask
 
+celery = None
 
 def celery_init_app(app: Flask) -> Celery:
-    class FlaskTask(Task):
+    class ContextTask(Task):
         def __call__(self, *args: object, **kwargs: object) -> object:
             with app.app_context():
                 return self.run(*args, **kwargs)
 
-    celery_app = Celery(app.name, task_cls=FlaskTask)
-    celery_app.config_from_object(app.config["CELERY"])
-    celery_app.set_default()
-    app.extensions["celery"] = celery_app
-    return celery_app
+    celery = Celery(app.name, task_cls=ContextTask)
+    celery.config_from_object(app.config["CELERY"])
+    celery.set_default()
+    app.extensions["celery"] = celery
+    return celery
